dbb2e96fefe1bd0769bfe6fd9af6ae1c514e6474
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-12-25 23:18:33 +0300

    Task 10 : Add JpaSpecificationExecutor to EmployeeRepository

--- src/main/java/com/springboot/jpaspecification/repository/EmployeeRepository.java
+++ src/main/java/com/springboot/jpaspecification/repository/EmployeeRepository.java
@@ -2,7 +2,8 @@ package com.springboot.jpaspecification.repository;
 
 import com.springboot.jpaspecification.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface  EmployeeRepository extends JpaRepository<Employee,Long> {
+public interface EmployeeRepository extends JpaRepository<Employee,Long>, JpaSpecificationExecutor<Employee> {
 
 }
