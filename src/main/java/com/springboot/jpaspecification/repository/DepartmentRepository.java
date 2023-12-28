b604aeae36a329216d0c63e4a20f70939ea1bcb1
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-12-28 22:54:17 +0300

    Task 23 : Add Java Doc for EmployeeRepository and DepartmentRepository

--- src/main/java/com/springboot/jpaspecification/repository/DepartmentRepository.java
+++ src/main/java/com/springboot/jpaspecification/repository/DepartmentRepository.java
@@ -3,6 +3,10 @@ package com.springboot.jpaspecification.repository;
 import com.springboot.jpaspecification.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Repository interface for performing CRUD operations on {@link Department} entities.
+ * Extends the Spring Data JPA {@link JpaRepository} interface, providing methods for common data access operations.
+ */
 public interface DepartmentRepository extends JpaRepository<Department,Long> {
 
 }
--- src/main/java/com/springboot/jpaspecification/repository/EmployeeRepository.java
+++ src/main/java/com/springboot/jpaspecification/repository/EmployeeRepository.java
@@ -4,6 +4,11 @@ import com.springboot.jpaspecification.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+/**
+ * Repository interface for performing CRUD and specification-based operations on {@link Employee} entities.
+ * Extends the Spring Data JPA {@link JpaRepository} interface for common CRUD operations,
+ * and the {@link JpaSpecificationExecutor} interface for handling specification-based queries.
+ */
 public interface EmployeeRepository extends JpaRepository<Employee,Long>, JpaSpecificationExecutor<Employee> {
 
 }
