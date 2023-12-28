903c4b37197b31c0fb3148c7b5b004224af20e69
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-12-28 22:56:31 +0300

    Task 24 : Add Java Doc for EmployeeService and EmployeeServiceImpl

--- src/main/java/com/springboot/jpaspecification/service/EmployeeService.java
+++ src/main/java/com/springboot/jpaspecification/service/EmployeeService.java
@@ -5,7 +5,17 @@ import com.springboot.jpaspecification.entity.Employee;
 
 import java.util.List;
 
+/**
+ * Service interface {@link EmployeeService} for managing and retrieving employee-related data.
+ */
 public interface EmployeeService {
 
+    /**
+     * Retrieves a list of employees based on the provided criteria in the {@link ListEmployeeRequest} object.
+     *
+     * @param listEmployeeRequest The request object containing criteria for filtering employees.
+     * @return A list of {@link Employee} objects that match the specified criteria.
+     */
     List<Employee> getEmployees(ListEmployeeRequest listEmployeeRequest);
+
 }
--- src/main/java/com/springboot/jpaspecification/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/springboot/jpaspecification/service/impl/EmployeeServiceImpl.java
@@ -9,14 +9,25 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * Implementation of the {@link EmployeeService} interface providing business logic
+ * for managing and retrieving employee-related data.
+ */
 @Service
 @RequiredArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
 
     private final EmployeeRepository employeeRepository;
 
+    /**
+     * Retrieves a list of employees based on the provided criteria in the {@link ListEmployeeRequest} object.
+     *
+     * @param listEmployeeRequest The request object containing criteria for filtering employees.
+     * @return A list of {@link Employee} objects that match the specified criteria.
+     */
     @Override
     public List<Employee> getEmployees(ListEmployeeRequest listEmployeeRequest) {
         return employeeRepository.findAll(listEmployeeRequest.toSpecification());
     }
+
 }
